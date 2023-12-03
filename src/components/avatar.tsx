34474204680419e72c991b98384b5592231ab3ac
naoufal bardouni <bardouni.naoufal@gmail.com>
2023-12-03 20:06:12 +0100

    fix avatar / adding default state

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,8 +2,9 @@ import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from "class-variance-authority"
 
 type Props = {
-	src: string
+	src: string|null|undefined
 	className?: string
+	fallback?: string
 } & VariantProps<typeof buttonVariants>
 
 const buttonVariants = cva(
@@ -22,13 +23,17 @@ const buttonVariants = cva(
 
 export function Avatar(props: Props){
 	return (
-		<div className={cn("relative inline-block rounded-full", buttonVariants({size: props.size}), props.className)}>
+		<div className={cn("relative inline-block rounded-full bg-muted", buttonVariants({size: props.size}), props.className)}>
 			<img
 				className="w-full rounded-full"
 				src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" />
-			<img
-				className="w-full top-0 left-0 h-full rounded-full absolute object-cover"
-				src={props.src} />
+			{
+				props.src ?
+				<img
+					className="w-full top-0 left-0 h-full rounded-full absolute object-cover"
+					src={props.src} /> :
+					<div className={cn("absolute left-[50%] top-[50%] border border-1 translate-y-[-50%] translate-x-[-50%] text-lg")}>{props.fallback}</div>
+			}
 		</div>
 	)
 }
