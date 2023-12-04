0136b0bed2a30b8c52c97d331b415e741b2f4201
naoufal bardouni <bardouni.naoufal@gmail.com>
2023-12-04 21:33:06 +0100

    accept fallback as children

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,9 +2,10 @@ import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from "class-variance-authority"
 
 type Props = {
+	children?: React.ReactNode
 	src: string|null|undefined
 	className?: string
-	fallback?: string | React.ReactElement
+	fallback?: string
 } & VariantProps<typeof avatarVariants>
 
 const avatarVariants = cva(
@@ -31,17 +32,17 @@ const avatarVariants = cva(
 export function Avatar(props: Props){
 	const corners = avatarVariants({corners: props.corners});
 	return (
-		<div className={cn("relative border border-neutral-400 rounded-lg inline-block bg-muted", avatarVariants({size: props.size}), corners, props.className)}>
+		<div className={cn("relative inline-block bg-muted", avatarVariants({size: props.size}), corners, props.className)}>
 			<img
-				className={cn("w-full bg-white", corners)}
+				className={cn("w-full", corners)}
 				src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" />
 			{
 				props.src ?
 				<img
 					className={cn("w-full top-0 left-0 h-full absolute object-cover", corners)}
-					src={props.src} 
-				/> :
-					<div className={cn(`absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] ${typeof props.fallback === 'string' ? 'text-lg' : ''}`)}>{props.fallback}</div>
+					src={props.src} /> :
+						props.children ??
+							<div className={cn("absolute left-[50%] top-[50%] border border-1 translate-y-[-50%] translate-x-[-50%] text-lg")}>{props.fallback}</div>
 			}
 		</div>
 	)
