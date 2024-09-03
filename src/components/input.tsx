0c5306832553317bf78de855f43fcae45012949e
omarhmitoch <60047489+omarhmitoch@users.noreply.github.com>
2024-09-03 12:16:55 +0100

    input opacity fix

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 		return (
 			<label
 				data-state={props.disabled ? "disabled" : undefined}
-				className={cn(`items-center block gap-1.5 relative data-[state=disabled]:cursor-not-allowed data-[state=disabled]:opacity-50`, className)}
+				className={cn(`items-center block gap-1.5 relative data-[state=disabled]:cursor-not-allowed data-[state=disabled]:opacity-80`, className)}
 			>
 				<div className={cn(initialInputClassName, inputContainerClassName ?? '')}>
 					{leftIcon}
